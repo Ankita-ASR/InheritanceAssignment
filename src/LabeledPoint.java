c059115dd6ae069c97a9b5ae6a7081cee806abc9
Ankita Saha <ankita7saha@gmail.com>
2024-10-30 16:51:51 +0600

    E-9.16

---
+++ src/LabeledPoint.java
@@ -0,0 +1,16 @@
+public class LabeledPoint {
+    private int x;
+    private int y;
+    private String label;
+
+    public LabeledPoint(int x, int y, String label) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ") - " + label;
+    }
+}
