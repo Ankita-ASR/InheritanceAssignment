d2614954683d163c2a26331c3cbba85ffab810a4
Ankita Saha <ankita7saha@gmail.com>
2024-10-30 17:09:31 +0600

    E-9.12

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    private String name;
+    private int yearOfBirth;
+
+    public Person(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", born in " + yearOfBirth;
+    }
+}
+
+class Student extends Person {
+    private String major;
+
+    public Student(String name, int yearOfBirth, String major) {
+        super(name, yearOfBirth);
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Major: " + major;
+    }
+}
+
+class Instructor extends Person {
+    private double salary;
+
+    public Instructor(String name, int yearOfBirth, double salary) {
+        super(name, yearOfBirth);
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Salary: $" + salary;
+    }
+}
